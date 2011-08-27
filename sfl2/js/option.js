9ff2bfef9e84d38117621c37dfde0fa7ab81955e
kyohei8 <tsukuda.kyouhei@gmail.com>
2011-08-27 18:48:31 +0900

    add all

---
+++ sfl2/js/option.js
@@ -0,0 +1,134 @@
+var jsonURL = "OptionItems.json";
+//保存
+function save_options() {
+	$.getJSON(jsonURL,function(json) {
+		for (var i in json) {
+			for (var j in json[i].data) {
+				localStorage[json[i].data[j].id] = $('#' + json[i].data[j].id).attr('checked');
+			}
+		}
+	});
+	
+	// Update status to let user know options were saved.
+	var status = document.getElementById("status");
+	status.innerHTML = "Options Saved.";
+	setTimeout(function() {
+		status.innerHTML = "";
+	}, 750);
+}
+
+// 復元
+function restore_options() {
+	$.getJSON(jsonURL,function(json) {
+		for (var i in json) {
+			for (var j in json[i].data) {
+				var checked = localStorage[json[i].data[j].id];
+				if (!checked) {
+					continue;
+				}
+				if (checked === 'checked') {
+					$('#' + json[i].data[j].id).attr('checked', 'checked');
+				}
+			}
+		}
+	});
+}
+
+//初期処理
+$(document).ready(function() {
+	var d = '';
+	var ib= '';
+	$.getJSON(jsonURL,function(json) {
+		for (var i in json) {
+			ib = 'itemBox-' + json[i].categoryId;
+			d += createSection(json[i].categoryName,json[i].categoryId, ib );
+			d += "<div class='itembox' id='" + ib + "'>";
+			for (var j in json[i].data) {
+				d += createRow(json[i].data[j].id, json[i].data[j].name, json[i].data[j].linkURL);
+			}
+			d += "</div>";
+		}
+		$("#visible-opt-table").html(d);
+		return;
+	})
+});
+
+/**
+ * item
+ * @param {Object} id
+ * @param {Object} name
+ * @param {Object} url
+ */
+function createRow(id,name,url){
+	var item = '<div class="item"><label>';
+	item += '<input type="checkbox" id="' + id + '">';
+	item += '<span class="visible-opt-name">' + name + '</span>';
+	item += '<div style="color: rgb(187, 187, 187); padding-left: 20px; margin-bottom: 5px; ">' + url + '</div>';
+	item += '</label></div>';
+	return item
+}
+
+/**
+ * セクションを作成
+ * @param {Object} title
+ */
+function createSection(title,id){
+	var scItem = '<div id="separatorItem" class="sectionheader">';
+	scItem += '<a id="' + id + '" class="" onclick="folding(\'' + id +'\')" >';
+	scItem += '<img src="image/plus-grey.png" id="plus-' + id + '" align="left" class="img" style="display:none"/>';
+	scItem += '<img src="image/minus-grey.png" id="minus-' + id + '" align="left" class="img"/>';
+	scItem += '<h5>' + title + '</h5></a></div>';
+	return scItem;
+}
+
+
+/**
+ * 折りたたみに関するJs
+ */
+function folding(id){
+	$("#plus-"+id).toggle();
+	$("#minus-"+id).toggle();
+	$("#itemBox-"+id).toggle();
+}
+
+function expand(id){
+	$("#plus-"+id).hide();
+	$("#minus-"+id).show();
+	$("#itemBox-"+id).show();
+}
+
+function collapse(id){
+	$("#plus-"+id).show();
+	$("#minus-"+id).hide();
+	$("#itemBox-"+id).hide();
+}
+function expandAll(){
+	$.getJSON(jsonURL,function(json) {
+		for (var i in json) {
+			 expand(json[i].categoryId);
+		}
+	})
+}
+function collapseAll(){
+	$.getJSON(jsonURL,function(json) {
+		for (var i in json) {
+			 collapse(json[i].categoryId);
+		}
+	})
+}
+
+/*check*/
+function checkall(isCheck){
+	$.getJSON(jsonURL,function(json) {
+		for (var i in json) {
+			for (var j in json[i].data) {
+				if(isCheck){				
+					$('#' + json[i].data[j].id).attr('checked', 'checked');
+				}else{
+					$('#' + json[i].data[j].id).removeAttr('checked');
+				}
+			}
+		}
+	})
+}
+
---
+++ sfl2/js/popup.js
@@ -0,0 +1,75 @@
+﻿/** popup.js */
+var c = chrome;
+var ws = c.windows;
+var tbs = c.tabs;
+ 
+$(function() {
+	ws.getCurrent(function (window) {
+		tbs.getSelected(window.id, function (tab) {
+			
+			$('#optionItem').click(function(){
+				tbs.create({ url: 'options.html'});
+			});
+			
+			var url = tab.url;
+			var domain = url.split('/')[2];
+			var sfUrl = 'https://' + domain;
+			if(sfUrl.indexOf('force.com') == -1){
+				//alert('Salesforceぢゃないよ');
+				$('#main').before("not Salesforce!");
+				return false;
+			}
+			//load json
+			var jsonURL = "OptionItems.json";
+			var isExist;
+			$.getJSON(jsonURL, function(json){
+				for (var i in json) {
+					var section = createSection(json[i].categoryName);
+					$('#main').append(section);
+					isExist = false;
+					for (var j in json[i].data) {
+						var checked = localStorage[json[i].data[j].id];
+						if (checked === 'checked') {
+							var div1 = createItem(json[i].data[j].id, json[i].data[j].name, sfUrl, json[i].data[j].linkURL);
+							$('#main').append(div1);
+							isExist = true;
+						}
+					}
+					if(!isExist){
+						$('#main div').remove(":last-child");
+					}
+				}
+				return;
+			}) 
+			
+		});
+	});
+});
+
+//create Items
+function createItem(id,name,sfUrl,url){
+	var d1 =$(document.createElement('div'));
+	d1.addClass('items');
+	var a1=document.createElement('a');
+	a1.setAttribute('id',id);
+	a1.setAttribute('target','_blank');
+	a1.appendChild(document.createTextNode(name));
+	a1.setAttribute('href', sfUrl + url);
+	d1.append(a1);
+	return d1;
+}
+
+/**
+ * セクションを作成
+ * @param {Object} title
+ */
+function createSection(title){
+	var span1=document.createElement('span');
+	var txt1=document.createTextNode(title);
+	span1.appendChild(txt1);
+	var div1=document.createElement('div');
+	div1.setAttribute('id','separatorItem');
+	div1.className='separator';
+	div1.appendChild(span1);
+	return div1;
+}
